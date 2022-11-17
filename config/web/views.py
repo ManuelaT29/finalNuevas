38ae48875a43c52e85485e635f04342321ef6d5d
Manuela Taborda <manuelataborda48@gmail.com>
2022-11-17 06:26:41 -0500

    Se crea MVC Empleados

---
+++ config/web/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render
+
+
+#IMPORTAR EL FORMULARIO A RENDER
+from web.formularios.formularioPlatos import FormularioPlatos
+from web.formularios.formularioEmpleados import FormularioEmpleados
+
+from web.models import Platos,Empleados
+
+# Create your views here.
+#las vistas en djangp son los CONTROLADORES
+
+#las vistas son funciones de python
+
+def Home(request):
+    return render(request,'index.html')
+
+def VistaPlatos(request):
+
+    formulario=FormularioPlatos()
+    datosParaTemplate={
+        'formularioPlato':formulario,
+        'bandera':False
+    }
+
+
+    #preguntamos si existe alguna peticion de tipo POST,asociada a la vista 
+
+    if request.method=='POST':
+        #deveriamos capturar los datos del fomrulario
+        datosDelFormulario=FormularioPlatos(request.POST)
+        #verificar si los datos llegaron correctamente (VALIDACIONES OK)
+        if datosDelFormulario.is_valid():
+            #capturar data
+            datosPlato=datosDelFormulario.cleaned_data
+            #Creamos un objeto del tipo MODELO PLATO
+            platoNuevo=Platos(
+                nombre=datosPlato["nombrePlato"],
+                descripcion=datosPlato["descripcionPlato"],
+                foto=datosPlato["fotoPlato"],
+                precio=datosPlato["precioPlato"],
+                tipo=datosPlato["tipoPlato"]
+            )
+
+            #Intentamos llevar el objeto plato nuevo a La BD
+            try:
+                platoNuevo.save()
+                datosParaTemplate["bandera"]=True
+                print("EXITO GUARDANDO LOS DATOS")
+
+            except Exception as error:
+                datosParaTemplate["bandera"]=False
+                print(error,error)
+
+    return render(request,'platos.html',datosParaTemplate)
+#---------------------------------------------------------------------------------------------------------------------------
+
+def VistaEmpleados(request):
+
+    formulario=FormularioEmpleados()
+    datosParaTemplate={
+        'formularioEmpleados':formulario,
+        'bandera':False
+    }
+
+
+    #preguntamos si existe alguna peticion de tipo POST,asociada a la vista 
+
+    if request.method=='POST':
+        #deveriamos capturar los datos del fomrulario
+        datosDelFormulario=FormularioEmpleados(request.POST)
+        #verificar si los datos llegaron correctamente (VALIDACIONES OK)
+        if datosDelFormulario.is_valid():
+            #capturar data
+            datosEmpleado=datosDelFormulario.cleaned_data
+            #Creamos un objeto del tipo MODELO PLATO
+            empleadoNuevo=Empleados(
+                nombre=datosEmpleado["nombreEmpelado"],
+                apellido=datosEmpleado["apellidoEmpelado"],
+                foto=datosEmpleado["fotoEmpelado"],
+                cargo=datosEmpleado["cargoEmpleado"],
+                salario=datosEmpleado["salarioEmpelado"],
+                contacto=datosEmpleado["contactoEmpleado"],
+
+            )
+
+            #Intentamos llevar el objeto plato nuevo a La BD
+            try:
+                empleadoNuevo.save()
+                datosParaTemplate["bandera"]=True
+                print("EXITO GUARDANDO LOS DATOS")
+
+            except Exception as error:
+                datosParaTemplate["bandera"]=False
+                print(error,error)
+
+    return render(request,'empleados.html',datosParaTemplate)
+
+
+
+
